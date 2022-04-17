e527bce3490251dc65e1bc8eace2be4e7cfb7c96
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 15:07:04 +0600

    IsNotSame

---
+++ src/Validation/Validators/IsNotSame.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-layer
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service\Validation\Validators;
+
+use Falseclock\Service\Validation\ValidationException;
+
+class IsNotSame extends IsSame
+{
+    /**
+     * @param null $value
+     * @param bool|null $nullable
+     * @return bool
+     * @throws ValidationException
+     */
+    public function check($value = null, ?bool $nullable = false): bool
+    {
+        return !parent::check($value, $nullable);
+    }
+}
