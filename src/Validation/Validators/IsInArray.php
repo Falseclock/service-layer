3b4552aac03842604316ba6e15682ca83d2bd45d
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 14:49:07 +0600

    IsInArray

---
+++ src/Validation/Validators/IsInArray.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-layer
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service\Validation\Validators;
+
+use Falseclock\Service\Validation\ValidationException;
+use Falseclock\Service\Validation\ValidatorImpl;
+
+class IsInArray extends ValidatorImpl
+{
+    public const ERROR_MULTI_DIMENSIONAL = "Array must not be multi dimensional";
+    public const ERROR_NO_ARRAY_DEFINED = "No haystack array defined";
+    /** @var array */
+    protected $haystack;
+
+    /**
+     * @param null $value
+     * @param bool|null $nullable
+     * @return bool
+     * @throws ValidationException
+     */
+    public function check($value = null, ?bool $nullable = false): bool
+    {
+        if (is_null($value) && $nullable) {
+            return true;
+        }
+
+        if (!is_array($this->haystack))
+            throw new ValidationException(self::ERROR_NO_ARRAY_DEFINED);
+
+        return in_array($value, $this->haystack, true);
+    }
+
+    /**
+     * @param array $array
+     *
+     * @return $this
+     * @throws ValidationException
+     */
+    public function haystack(array $array): IsInArray
+    {
+        // Check for multi dimensional array
+        if (count($array) != count($array, COUNT_RECURSIVE)) {
+            throw new ValidationException(self::ERROR_MULTI_DIMENSIONAL);
+        }
+
+        $this->haystack = $array;
+
+        return $this;
+    }
+}
