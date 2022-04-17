7ffa903b071a40b8b1ee53e281eef9c200daebfe
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 15:02:14 +0600

    IsNotNull

---
+++ src/Validation/Validators/IsNotNull.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-layer
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service\Validation\Validators;
+
+use Falseclock\Service\Validation\ValidatorImpl;
+
+class IsNotNull extends ValidatorImpl
+{
+    /**
+     * @param null $value
+     * @param bool|null $nullable
+     * @return bool
+     */
+    public function check($value = null, ?bool $nullable = false): bool
+    {
+        return !is_null($value);
+    }
+}
