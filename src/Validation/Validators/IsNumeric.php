75391ec16aeb9483a500ffdc9ada8e332c0a25af
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 15:11:17 +0600

    IsNumeric

---
+++ src/Validation/Validators/IsNumeric.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-layer
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service\Validation\Validators;
+
+use Falseclock\Service\Validation\ValidatorImpl;
+
+class IsNumeric extends ValidatorImpl
+{
+    /**
+     * @param $value
+     * @param bool|null $nullable
+     * @return bool
+     */
+    public function check($value = null, ?bool $nullable = false): bool
+    {
+        if (is_null($value) && $nullable) {
+            return true;
+        }
+        return is_numeric($value);
+    }
+}
