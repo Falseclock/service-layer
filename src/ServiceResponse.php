b5d95e321d2ef64198a36e02f8ba3fd91fb32633
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 10:26:50 +0600

    initial commit

---
+++ src/ServiceResponse.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-request
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service;
+
+use Falseclock\Service\Validation\ValidatorError;
+
+class ServiceResponse
+{
+    /** @var ValidatorError[] */
+    protected $requestErrors = [];
+    /** @var ServiceError[] */
+    protected $serviceErrors = [];
+    /** @var mixed */
+    protected $result;
+
+    public function __construct($result, array $serviceErrors = [], array $requestErrors = [])
+    {
+        $this->result = $result;
+        $this->serviceErrors = $serviceErrors;
+        $this->requestErrors = $requestErrors;
+    }
+
+    /**
+     * @return ServiceError[]
+     */
+    public function getServiceErrors(): array
+    {
+        return $this->serviceErrors;
+    }
+
+    /**
+     * Check's whether request values are valid
+     *
+     * @return bool
+     */
+    public function isRequestValid(): bool
+    {
+        return count($this->requestErrors) == 0;
+    }
+
+    /**
+     * Check's whether request values are NOT valid
+     *
+     * @return bool
+     */
+    public function isRequestInValid(): bool
+    {
+        return count($this->requestErrors) != 0;
+    }
+
+    /**
+     * @return ValidatorError[]
+     */
+    public function getRequestErrors(): array
+    {
+        return $this->requestErrors;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * @param int $errorCode
+     * @return bool
+     */
+    public function hasServerError(int $errorCode): bool
+    {
+        foreach ($this->serviceErrors as $error) {
+            if ($error->getCode() == $errorCode) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
