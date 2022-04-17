13222cf50d95cd96c052c29ce1a9f24ab2eb0d47
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 10:35:50 +0600

    ServiceError.php

---
+++ src/ServiceError.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-request
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service;
+
+class ServiceError
+{
+    public const ERROR_INVALID_REQUEST = 1000000;
+    public const ERROR_UNEXPECTED = 1000001;
+    /** @var int */
+    protected $code;
+    /** @var string */
+    protected $message;
+
+    public function __construct(string $message, int $code = 0)
+    {
+        $this->message = $message;
+        $this->code = $code;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
