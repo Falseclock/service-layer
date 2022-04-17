3259a0d4944b234b2c3d90f381ef0fb95c678512
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 13:14:01 +0600

    IsSame

---
+++ src/Validation/Validators/IsSame.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-layer
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service\Validation\Validators;
+
+use Falseclock\Service\Validation\ValidationException;
+use Falseclock\Service\Validation\ValidatorImpl;
+
+class IsSame extends ValidatorImpl
+{
+    public const ERROR_NO_MATCH = "No value to compare. Use 'against' function.";
+    /** @var mixed */
+    protected $against;
+
+    /**
+     * @param mixed $value
+     * @return $this
+     */
+    public function against($value): IsSame
+    {
+        $this->against = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $value
+     * @param bool|null $nullable
+     * @return bool
+     * @throws ValidationException
+     */
+    public function check($value = null, ?bool $nullable = false): bool
+    {
+        if (is_null($value) && $nullable) {
+            return true;
+        }
+
+        if (!isset($this->against))
+            throw new ValidationException(self::ERROR_NO_MATCH);
+
+        if ($value === $this->against) {
+            return true;
+        }
+
+        return false;
+    }
+}
