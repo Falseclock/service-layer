006d960f351af83ff5a59e42b593d82caa217b6e
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 11:12:52 +0600

    ValidationException

---
+++ src/Validation/ValidationException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2022 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/service-request
+ */
+
+declare(strict_types=1);
+
+namespace Falseclock\Service\Validation;
+
+use Exception;
+use Throwable;
+
+class ValidationException extends Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
