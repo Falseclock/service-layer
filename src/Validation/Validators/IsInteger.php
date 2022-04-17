10b476b5858071da477754bcf81854f35e0bbe1d
Nurlan Mukhanov <nurike@gmail.com>
2022-04-17 15:25:43 +0600

    IsInteger

--- src/Validation/Validators/IsInteger.php
+++ src/Validation/Validators/IsInteger.php
@@ -10,20 +10,29 @@ declare(strict_types=1);
 
 namespace Falseclock\Service\Validation\Validators;
 
+use Falseclock\Service\Validation\ValidationException;
 use Falseclock\Service\Validation\ValidatorImpl;
 
 class IsInteger extends ValidatorImpl
 {
+    public const ERROR_FLOAT_PROVIDED = "IsInteger class should not validate float values";
+
     /**
      * @param $value
      * @param bool|null $nullable
      * @return bool
+     * @throws ValidationException
      */
     public function check($value = null, ?bool $nullable = false): bool
     {
         if (is_null($value) && $nullable) {
             return true;
         }
+
+        if (is_float($value)) {
+            throw new ValidationException(self::ERROR_FLOAT_PROVIDED);
+        }
+
         return is_integer($value);
     }
 }
